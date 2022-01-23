bc514b55839799edfbea63357fce6620ea6b6b07
GiovanniKamarowski <95241151+GiovanniKamarowski@users.noreply.github.com>
2022-01-23 17:13:28 -0300

    Exercício 1
    "Fazer um programa para ler um número inteiro N e depois os dados (id, nome e salario) de
    N funcionários. Não deve haver repetição de id.
    Em seguida, efetuar o aumento de X por cento no salário de um determinado funcionário.
    Para isso, o programa deve ler um id e o valor X. Se o id informado não existir, mostrar uma
    mensagem e abortar a operação. Ao final, mostrar a listagem atualizada dos funcionários,
    conforme exemplos.
    Lembre-se de aplicar a técnica de encapsulamento para não permitir que o salário possa
    ser mudado livremente. Um salário só pode ser aumentado com base em uma operação de
    aumento por porcentagem dada."

---
+++ lista/src/application/Program.java
@@ -0,0 +1,71 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employees;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+
+		List<Employees> list = new ArrayList<>();
+
+		System.out.println("How many employees will be registred? ");
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("");
+			System.out.println("Employoee #" + (i + 1) + ":");
+			System.out.print("Id: ");
+			Integer id = sc.nextInt();
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Wage: ");
+			double wage = sc.nextDouble();
+
+			Employees emp = new Employees(id, name, wage);
+
+			list.add(emp);
+
+		}
+
+		System.out.println("Enter the employee id that will have salary increase: ");
+		int idwage = sc.nextInt();
+		// Employees emp = list.stream().filter(x -> x.getId() ==
+		// ).findFirst().orElse(null);
+		Integer pos = position(list, idwage);
+		if (pos == null) {
+			System.out.println("");
+			System.out.println("Id not found");
+		} else {
+			System.out.print("Enter the percentage: ");
+			double percent = sc.nextDouble();
+			list.get(pos).increaseWage(percent);
+		}
+		System.out.println("");
+		System.out.println("List of employees: ");
+		for (Employees emp : list) {
+			System.out.println(emp);
+		}
+
+		sc.close();
+
+	}
+
+	public static Integer position(List<Employees> list, int id) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getId() == id) {
+				return i;
+			}
+		}
+		return null;
+	}
+
+}
---
+++ lista/src/entities/Employees.java
@@ -0,0 +1,52 @@
+package entities;
+
+public class Employees {
+
+	private int id;
+	private String name;
+	private double wage;
+
+	public Employees() {
+
+	}
+
+	public Employees(int id, String name, double wage) {
+		this.id = id;
+		this.name = name;
+		this.wage = wage;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getWage() {
+		return wage;
+	}
+
+	public void setWage(double wage) {
+		this.wage = wage;
+	}
+
+	public void increaseWage(double percentage) {
+		wage += wage * percentage / 100.0;
+	}
+
+	@Override
+	public String toString() {
+		return "Employees [id= " + id + ", name= " + name + ", " + String.format("%.2f", wage) + "]";
+	}
+
+}
